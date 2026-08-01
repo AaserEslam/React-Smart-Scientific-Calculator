277357cdd371b25b0cc53f3872b79edeb776cccb
Aaser Eslam <gameraaser2011@gmail.com>
2026-08-01 15:10:41 +0300

    Created Vite & Tailwind

---
+++ Calculator/src/Components/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <div>
+        <div className='bg-gray-400 h-[500px] w-'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Calculator
