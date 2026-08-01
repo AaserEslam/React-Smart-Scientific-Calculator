7d0a4e0b538bb8277237bb5d79786debfc94006b
Aaser Eslam <gameraaser2011@gmail.com>
2026-08-01 15:48:21 +0300

    Created The Input Box

--- Calculator/src/Components/Calculator.jsx
+++ Calculator/src/Components/Calculator.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import InputNum from './InputNum'
+import Buttons from './Buttons'
 
 const Calculator = () => {
   return (
     <div>
-        <div className='bg-gray-400 rounded-lg h-130 w-100 p-5'>
-            <div className='bg-red-600'>
-                <input type="text" />
+        <div className='bg-gray-900 rounded-lg h-130 w-100 p-5'>
+            <div>
+                <InputNum/>
+            </div>
+            <div className='h-75 mt-10'>
+                <Buttons/>
             </div>
         </div>
     </div>
--- Calculator/src/Components/InputNum.jsx
+++ Calculator/src/Components/InputNum.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const InputNum = () => {
   return (
-    <div>InputNum</div>
+    <div>
+        <div>
+            <input type="text" className='w-full h-25 outline-0 border-none  text-right text-white font-semibold text-5xl px-3'/>
+        </div>
+        <div>
+            <input type="text" className='w-full h-10 outline-0 border-none font-medium  text-right text-gray-400 px-3 text-lg'/>
+        </div>
+    </div>
   )
 }
 
